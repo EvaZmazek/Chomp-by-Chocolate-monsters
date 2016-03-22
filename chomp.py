bffa0c71fb8969ed0e6a3b4f3d460283f04175e9
ZivaUrbancic <ziva.urbancic@student.fmf.uni-lj.si>
2016-03-23 00:06:52 +0100

    Threading
    Implementacija in prilagoditev funkcij.

--- chomp.py
+++ chomp.py
@@ -1,16 +1,24 @@
 from tkinter import *
 from random import *
+import threading
+#import argparse - kasneje bova rabili
+import logging
+
 SIRINA=7
 VISINA=5
 IGRALEC_1 = "1"
 IGRALEC_2 = "2"
 NI_KONEC = "ni konec"
 
+MINIMAX_GLOBINA = 3
+
 def nasprotnik(igralec):
     if igralec == IGRALEC_1:
         return IGRALEC_2
-    else:
+    elif igralec == IGRALEC_2:
         return IGRALEC_1
+    else:
+        assert False, "neveljaven nasprotnik"
 
     
 
@@ -35,6 +43,12 @@ class Igra():
         self.zgodovina.append((p, self.na_potezi))
         #print("Izpisujem zgodovino:")
         #print(self.zgodovina)
+
+    def kopija(self):
+        k=Igra()
+        k.koscki = [self.koscki[i][:] for i in range(VISINA)]
+        k.na_potezi = self.na_potezi
+        return k
 
     def veljavne_poteze(self):
         poteze=[]
@@ -95,19 +109,117 @@ class Clovek():
         #Čakamo na to, da igralec klikne na ploščo
         pass
 
+    def prekini(self):
+        pass
+
     def klik(self, i, j):
         self.gui.povleci_potezo(i, j)
         # povleci_potezo je tu metoda v razredu Gui(), ki kliče metodo povleci_potezo v razredu Igra()         
+
+
+#######################################################################
+## Razred Racunalnik:
+class Racunalnik():
+    def __init__(self, gui, algoritem):
+        self.gui = gui
+        self.algoritem = algoritem
+        self.mislec = None
+
+    def igraj(self):
+        self.mislec = threading.Thread(
+            target = lambda: self.algoritem.izracunaj_potezo(self.gui.igra.kopija()))
+
+        self.mislec.start()
+
+        self.gui.plosca.after(100, self.preveri_potezo)
+
+    def preveri_potezo(self):
+        if self.algoritem.poteza is not None:
+            i, j = self.algoritem.poteza
+            self.gui.povleci_potezo(i,j)
+            self.mislec = None
+            #ustavi razmišljanje - "ubije" thread
+        else:
+            self.gui.plosca.after(100, self.preveri_potezo)
+
+    def prekini(self):
+        if self.mislec:
+            logging.debug("Prekinjam {0}".format(self.mislec))
+            self.algoritem.prekini()
+            self.mislec.join() # Počakamo, da se vlakno ustavi
+            self.mislec = None
+
+    def klik(self, i, j):
+        pass
+        # povleci_potezo je tu metoda v razredu Gui(), ki kliče metodo
+        # povleci_potezo v razredu Igra()
+        
+
+#######################################################################
+## Minimax:
+class Minimax:
+    def __init__(self, globina): # ne sme operirati z gui-em
+        self.globina = globina # do katere globine iščemo
+        self.prekinitev = False # ali je bila klicana funkcija prekini?
+        self.igra = None # objekt, ki opisuje igro
+        # Verjetno ne bova potrebovali: self.jaz = IGRALEC_2
+        self.poteza = None # sem shrani izračunano potezo
+
+    def prekini(self):
+        self.prekinitev = True
+
+    def izracunaj_potezo(self, igra):
+        pass
+
+    def vrednost_pozicije(self):
+        pass
+    
+    def minimax(self, globina, maksimiziramo):
+        pass
 
 #######################################################################
 ## Naključje:
 class Nakljucje():
     def __init__(self, gui):
         self.gui = gui
+        self.igra = None
+        self.poteza = None # tuple!!
+        self.prekinitev = False
 
-    def igraj(self):
-        i,j=self.zrebaj()
-        self.gui.povleci_potezo(i,j)
+    ZMAGA = 10000
+    NESKONCNO = ZMAGA + 1
+
+    def izracunaj_potezo(self, igra):
+        self.igra = igra
+        self.prekinitev = False
+        self.poteza = None
+        (poteza, vrednost) = self.nakljucje()
+        self.igra = None
+        if not self.prekinitev:
+            logging.debug("Nakljucje: poteza{0}".format(poteza))
+            self.poteza = poteza
+        #else:
+            #print("Klicana je bila prekinitev")
+            
+
+    def nakljucje(self):
+        #self.igra = igra
+        if self.prekinitev:
+            logging.debug("Nakljucje prekinja")
+            return (None, 0) # Pri nakljucju vrednost poteze ni pomembna.
+        stanje = self.igra.stanje_igre()
+        if stanje in (IGRALEC_1, IGRALEC_2):
+            """ Igre je konec. """
+            if stanje == IGRALEC_2:
+                return (None, 0)#Nakljucje.ZMAGA)
+            else:
+                return (None, 0)#-Nakljucje.ZMAGA)
+        elif stanje == NI_KONEC:
+            return (self.zrebaj(), 0)
+        else:
+            assert False, "Nakljucje: nedefinirano stanje igre"
+        #self.poteza = self.zrebaj() # tuple!!
+        #self.gui.povleci_potezo(i,j)
 
     def zrebaj(self):
         seznam=self.gui.igra.veljavne_poteze()
@@ -117,25 +229,8 @@ class Nakljucje():
         else:
             return seznam[0]
 
-    def klik(self, i, j):
-        pass
-        # povleci_potezo je tu metoda v razredu Gui(), ki kliče metodo povleci_potezo v razredu Igra()
-
-#######################################################################
-## Minimax:
-class Minimax():
-    def __init__(self,gui):
-        self.gui = gui
-
-    def igraj(self):
-        pass
-
-    def vrednost_pozicije(self):
-        pass
-
-    def klik(self, i, j):
-        pass
-        # povleci_potezo je tu metoda v razredu Gui(), ki kliče metodo povleci_potezo v razredu Igra()
+    def prekini(self):
+        self.prekinitev = True
 
 #######################################################################
 
@@ -151,9 +246,9 @@ class Gui():
         igra_menu = Menu(master)
         menu.add_cascade(label="Igra", menu=igra_menu)
 
-        igra_menu.add_command(label="2 igralca",command=lambda: self.nova_igra(Clovek))
+        igra_menu.add_command(label="2 igralca",command=lambda: self.nova_igra(Clovek(self)))
         # command mora bit funkcija
-        igra_menu.add_command(label="Proti racunalniku (easy)",command=lambda: self.nova_igra(Nakljucje))
+        igra_menu.add_command(label="Proti racunalniku (easy)",command=lambda: self.nova_igra(Racunalnik(self, Nakljucje(self))))
         igra_menu.add_command(label="Proti racunalniku (medium)")#, command=lambda: self.nova_igra(Minimax))
         igra_menu.add_command(label="Proti racunalniku (hard)")#,   command=#lambda: self.nova_igra(AlfaBeta))
         igra_menu.add_command(label="Izhod",                      command=master.destroy)
@@ -166,7 +261,7 @@ class Gui():
         self.plosca = Canvas(master, width=SIRINA*100 + 20, height=VISINA*100 + 60)
         self.plosca.grid(row=1, column=0)
         
-        self.nova_igra(Clovek)
+        self.nova_igra(Racunalnik(self, Nakljucje(self)))#Clovek(self))
         
         #Klik na polje
         self.plosca.bind("<Button-1>", self.plosca_klik)
@@ -206,7 +301,8 @@ class Gui():
 
         #Določimo igralce
         self.igralec_1 = Clovek(self)
-        self.igralec_2 = Igralec_2(self)
+        self.igralec_2 = Igralec_2
+        #print(type (self.igralec_2))
         
         #Narišemo polje:
         for i in range(VISINA):
@@ -229,7 +325,6 @@ class Gui():
         else:
             # Poteza je veljavna. V igri smo jo že potegnili, zdaj moramo spremeniti še prikaz.
             self.pobrisi(i,j)
-            #print("Klical sem pobrisi({0},{1})".format(i,j))
             if stanje == NI_KONEC:
                 # Potezo ima naslednji igralec. To moramo povedati na zaslonu in klicati metodo, da bo igral.
                 if self.igra.na_potezi == IGRALEC_1:
@@ -245,10 +340,8 @@ class Gui():
     def pobrisi(self,i,j):
         if i==0 and j==0:
             self.napis.set('raje pojej nezastrupljen košček čokolade')
-            #print("Ne brišem.")
         elif self.igra.koscki[j][i] is None:
             self.napis.set('ups, ta košček čokolade je nekdo že pojedel')
-            #print("Ne brišem, ker me je baje nekdo prehitel.")
         else:
             for k in range(VISINA-j):
                 for l in range(SIRINA-i):
